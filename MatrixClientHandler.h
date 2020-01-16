f006ad701cb6b117bd112c5d667afdd837864c7c
kfiryehuda <kfir.yehuda50@gmail.com>
2020-01-16 11:38:56 +0200

    im progress of matrix solver

--- MatrixClientHandler.h
+++ MatrixClientHandler.h
@@ -4,9 +4,23 @@
 
 #ifndef GENERICSOLVER_MATRIXCLIENTHANDLER_H
 #define GENERICSOLVER_MATRIXCLIENTHANDLER_H
+#include "ClientHandler.h"
+#include "Solver.h"
+#include "CacheManager.h"
 
 
-class MatrixClientHandler {
+class MatrixClientHandler  : public ClientHandler{
+private:
+    Solver<std::string,std::string>* solver;
+    CacheManager<std::string>* cm;
+public:
+    MatrixClientHandler(Solver<std::string, std::string> *pSolver){
+        solver = pSolver;
+    }
+
+    void handleClient(int socketFD, int outputStream) override{
+
+    }
 
 };
 
---
+++ MatrixSolver.h
@@ -0,0 +1,14 @@
+//
+// Created by kfiry on 16/01/2020.
+//
+
+#ifndef GENERICSOLVER_MATRIXSOLVER_H
+#define GENERICSOLVER_MATRIXSOLVER_H
+
+
+//class MatrixSolver : Solver<{
+//
+//};
+
+
+#endif //GENERICSOLVER_MATRIXSOLVER_H
