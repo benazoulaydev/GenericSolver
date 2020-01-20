b8d3e8c6a6fff1b7286696b60b1c32ae6f19b78b
kfiryehuda <kfir.yehuda50@gmail.com>
2020-01-20 20:00:34 +0200

    few fixed

--- BestFS.h
+++ BestFS.h
@@ -8,6 +8,7 @@
 #include "Solution.h"
 #include <bits/stdc++.h>
 using namespace  std;
+// better implemtation of the algorithm BestFirstSearch
 
 template <typename T>
 class BestFS : public ISearcher<T>{
@@ -38,6 +39,8 @@ public:
         return newTop;
     }
     Solution<T>* search(Searchable<T>* searchable) override;
+
+    virtual ~BestFS();
 };
 
 
@@ -56,6 +59,8 @@ Solution<T> *BestFS<T>::search(Searchable<T>* searchable) {
             s->setCost(s->getCost()+n->getCost());
             if(closed.find(s->getState()) == closed.end() && !openContains(s->getState())){
                 addToOpenList(s);
+            } else {
+                delete s;
             }
         }
     }
@@ -75,6 +80,13 @@ Solution<T> *BestFS<T>::backTrace(State<T> *goalState, State<T> *init) {
     return solution;
 }
 
+template<typename T>
+BestFS<T>::~BestFS() {
+    for(auto i1=closed.begin();i1!=closed.end();i1++) {
+        delete &i1->second;
+    }
+    cout<<"hi"<<endl;
+}
 
 
 #endif //GENERICSOLVER_BESTFS_H
